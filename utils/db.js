0e1aad4e118954ce5f729787fe9bcaf302feca35
colly-m <sindanicolly@gmail.com>
2024-06-30 08:22:50 -0700

    Mongodb utils udate

---
+++ utils/db.js
@@ -0,0 +1,38 @@
+import { MongoClient } from require('mongodb');
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
+    
+    this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    this.dbName = database;
+    
+    this.client.connect((err) => {
+      if (err) {
+        console.error('MongoDB client connection error:', err);
+      }
+    });
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const db = this.client.db(this.dbName);
+    const usersCollection = db.collection('users');
+    return usersCollection.countDocuments();
+  }
+
+  async nbFiles() {
+    const db = this.client.db(this.dbName);
+    const filesCollection = db.collection('files');
+    return filesCollection.countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
