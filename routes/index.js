fec792b4b6fb9407180d92e56f53de5ed2ebeadf
colly-m <sindanicolly@gmail.com>
2024-07-03 06:22:22 -0700

    New user creation

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ import { Router } from 'express';
 
 const router = Router();
 const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UserController.postNew);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from 'express';
+const bodypParser = require('body-parser');
 const routes = require('./routes');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
 app.use('/', routes);
 
 app.listen(port, () => {
